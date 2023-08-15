6a7e86f39ac9e561ac44f6f1893e456c6ec192a1
Mehul Mittal <mittal.freelance@gmail.com>
2023-08-15 12:59:32 +0000

    Implement subract using interface

---
+++ subtract.py
@@ -0,0 +1,18 @@
+from main import ArithmeticOpsInterface
+
+class SubtractOp(ArithmeticOpsInterface):
+    def add(self, a: int, b: int) -> int:
+        '''Add the given numbers'''
+        pass
+
+    def subtract(self, a: int, b: int) -> int:
+        '''Subtract b from a'''
+        return a-b
+
+    def multiply(self, a: int, b: int) -> int:
+        '''Multiply the given numbers'''
+        pass
+
+    def divide(self, a: int, b: int) -> int:
+        '''Divide a with b'''
+        pass
