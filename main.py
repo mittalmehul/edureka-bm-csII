37086c21de049350cd6669ea0c6f17a25b0192b2
Mehul Mittal <mittal.freelance@gmail.com>
2023-08-15 12:50:31 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class ArithmeticOpsInterface:
+    def add(self, a: int, b: int) -> int:
+        '''Add the given numbers'''
+        pass
+
+    def subtract(self, a: int, b: int) -> int:
+        '''Subtract b from a'''
+        pass
+
+    def multiply(self, a: int, b: int) -> int:
+        '''Multiply the given numbers'''
+        pass
+
+    def divide(self, a: int, b: int) -> int:
+        '''Divide a with b'''
+        pass
